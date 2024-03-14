912b988cb8e993ed894bf61dae34ae69c350e373
chenyin <2261665478@qq.com>
2024-03-14 23:45:09 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Hello world!");
+    }
+}
